91f6c1093a761d5df5e98c5731f4aa9a285e012b
ArejGris <mairygris123@gmail.com>
2024-05-26 20:56:04 +0300

    otp2

--- src/handlers/user/confirmUser.js
+++ src/handlers/user/confirmUser.js
@@ -3,7 +3,12 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const confirmUser=async(req,reply)=>{
 const user=req.body
+try {
+    
 await prisma.user.update({where:{id:user.id},data:{confirmNumber:true}})
 reply.send({status:200,message:"user confirmed"})
+} catch (error) {
+    reply.send({status:200,message:"internal server error"})
+}
 }
 module.exports=confirmUser
